7a4b51ae32c4ef6391aa2fccfcb03dd83f945f1c
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-07-08 18:45:46 +0100

    Allow setting the version to analyze in the command-line interface tool

--- cli.py
+++ cli.py
@@ -13,13 +13,14 @@ import crash_deviations
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Signature Correlations')
     parser.add_argument('signature', action='store', help='The signature you\'re interested in.')
+    parser.add_argument('version', action='store', help='The version you\'re interested in.')
 
     args = parser.parse_args()
 
     sc = SparkContext(appName='CrashCorrelations')
     sqlContext = SQLContext(sc)
 
-    df_a = download_data.get_crashes(sqlContext, version='47.0', days=2)
+    df_a = download_data.get_crashes(sqlContext, version=args.version, days=2)
     df_b = df_a.filter(df_a['signature'] == args.signature)
 
     crash_deviations.find_deviations(sc, df_a, df_b, min_support_diff=0.15, min_corr=0.03, max_addons=50)
