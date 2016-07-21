b232a246df4a557bd4596d1100f20e2a7dc048b3
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-07-22 01:40:59 +0200

    Increase font size in the plot.
    Fixes #8.

--- plot.py
+++ plot.py
@@ -3,12 +3,7 @@
 # License, v. 2.0. If a copy of the MPL was not distributed with this file,
 # You can obtain one at http://mozilla.org/MPL/2.0/.
 
-import numpy as np
 import matplotlib.pyplot as plt
-from matplotlib import rcParams
-
-
-rcParams.update({'figure.autolayout': True})
 
 
 def rule_to_str(rule):
@@ -24,17 +19,20 @@ def plot(results, label_a, label_b, outputFile=None):
     values_a = [100 * rule['support_a'] for rule in all_rules]
     values_b = [100 * rule['support_b'] for rule in all_rules]
 
+    plt.rc('figure', autolayout=True)
+    plt.rc('font', size=22)
+
     fig, ax = plt.subplots(figsize=(24, 18))
-    index = np.arange(len(all_rules))
+    index = range(len(all_rules))
     bar_width = 0.35
 
     plt.barh(index, values_a, bar_width, color='b', label=label_a)
-    plt.barh(index + bar_width, values_b, bar_width, color='r', label=label_b)
+    plt.barh([i + bar_width for i in index], values_b, bar_width, color='r', label=label_b)
 
     plt.xlabel('Support')
     plt.ylabel('Rule')
     plt.title('Most interesting deviations')
-    plt.yticks(index + bar_width, [rule_to_str(rule['item']) for rule in all_rules])
+    plt.yticks([i + bar_width for i in index], [rule_to_str(rule['item']) for rule in all_rules])
     plt.legend()
 
     if outputFile is not None:
