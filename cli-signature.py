b19ed97a2cd122ee2ced020dcd9fb59af6443a32
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-07-22 23:39:00 +0200

    Allow running cli-signature.py either via a set of versions or a channel

--- cli-signature.py
+++ cli-signature.py
@@ -13,15 +13,23 @@ import plot
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Signature Correlations')
     parser.add_argument('signature', action='store', help='The signature you\'re interested in.')
-    parser.add_argument('versions', action='store', nargs='+', help='The versions you\'re interested in.')
+
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-v', '--versions', action='store', nargs='+', help='The versions you\'re interested in.')
+    group.add_argument('-c', '--channel', action='store', help='The channel you\'re interested in.')
 
     args = parser.parse_args()
 
+    if args.channel:
+        versions = download_data.get_versions(args.channel)
+    else:
+        versions = args.versions
+
     sc = SparkContext(appName='CrashCorrelations')
 
-    download_data.download_crashes(versions=args.versions, days=5)
+    download_data.download_crashes(versions=versions, days=5)
 
-    df_a = crash_deviations.get_crashes(sc, versions=args.versions, days=5)
+    df_a = crash_deviations.get_crashes(sc, versions=versions, days=5)
     df_b = df_a.filter(df_a['signature'] == args.signature)
 
     results = crash_deviations.find_deviations(sc, df_a, df_b, min_support_diff=0.15, min_corr=0.03, max_addons=50)
