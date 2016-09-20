0438976d48f3c93ecc6edf454023c751e3c78d7e
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-09-20 13:27:02 +0100

    Add gfx_critical_errors module

---
+++ gfx_critical_errors.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this file,
+# You can obtain one at http://mozilla.org/MPL/2.0/.
+
+import argparse
+import re
+import requests
+
+
+def query_dxr(q):
+    r = requests.get('https://dxr.mozilla.org/mozilla-central/search', params={
+        'q': q,
+        'limit': 1000
+    }, headers={
+        'Accept': 'application/json'
+    })
+
+    if r.status_code != 200:
+        print(r.text)
+        raise Exception(r)
+
+    return r.json()
+
+
+def get_critical_errors():
+    results = query_dxr('gfxCriticalError(')['results'] + query_dxr('gfxCriticalNote <<')['results'] + query_dxr('gfxCriticalErrorOnce(')['results']
+
+    matches = [re.search(r'"(.*?)"', line['line']) for result in results for line in result['lines']]
+
+    errors = [match.group(1) for match in matches if match is not None]
+
+    return set([error for error in errors if error != ', '])
