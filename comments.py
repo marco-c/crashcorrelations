fddc0aaa08489c406330d248ad279c132bcd0c13
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-11-08 18:22:33 +0000

    Add a script to generate the top words used in comments in a given signature vs overall with tf-idf

---
+++ comments.py
@@ -0,0 +1,41 @@
+import re
+
+from pyspark.ml.feature import Tokenizer, StopWordsRemover, CountVectorizer, IDF
+from pyspark.sql import functions
+
+from stemming.porter2 import stem
+
+
+def get_top_words(dataset):
+    # TODO: Use stemmers for the languages supported by http://www.nltk.org/api/nltk.stem.html#nltk.stem.snowball.SnowballStemmer
+    # Or translate comments in other languages using the free Microsoft Translate API.
+    sentenceData = dataset.filter(dataset['user_comments'].isNotNull() & (dataset['useragent_locale'].isNull() | (functions.instr(dataset['useragent_locale'], 'en') == 1)))
+
+    # Tokenize comments.
+    tokenizer = Tokenizer(inputCol='user_comments', outputCol='words')
+    wordsData = tokenizer.transform(sentenceData)
+
+    # Remove duplicate words from comments.
+    wordsData = wordsData.rdd.map(lambda p: (p['signature'], list(set(p['words'])))).reduceByKey(lambda x, y: x + y).toDF(['signature', 'words'])
+
+    # Clean comment words by removing puntuaction and stemming.
+    def clean_word(w):
+        return re.sub('\,|\.|\;|\:|\;|\?|\!|\[|\]|\}|\{|\/|\\\\', '', stem(w.lower()))
+
+    wordsData = wordsData.rdd.map(lambda p: (p['signature'], [clean_word(w) for w in p['words']])).toDF(['signature', 'words'])
+
+    # XXX: Useless with TF-IDF?
+    remover = StopWordsRemover(inputCol='words', outputCol='filtered')
+    cleanWordsData = remover.transform(wordsData)
+
+    cv = CountVectorizer(inputCol='filtered', outputCol='features')
+    model = cv.fit(cleanWordsData)
+    featurizedData = model.transform(cleanWordsData)
+
+    idf = IDF(inputCol='features', outputCol='tfidf_features')
+    idfModel = idf.fit(featurizedData)
+    rescaledData = idfModel.transform(featurizedData)
+
+    bests_per_doc = rescaledData.rdd.map(lambda p: (p['signature'], sorted(zip(p['tfidf_features'].indices, p['tfidf_features'].values), key=lambda i: i[1], reverse=True)[:10])).collect()
+
+    return dict([(signature, [model.vocabulary[best] for best, val in bests]) for signature, bests in bests_per_doc])
