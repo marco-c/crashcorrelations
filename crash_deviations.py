55e6797d9ce3911b2d71441b6ca8244512509158
Marco Castelluccio <mcastelluccio@mozilla.com>
2016-12-19 17:25:49 +0100

    Add ipc_channel_error to the list of attributes affected by startup_crash

--- crash_deviations.py
+++ crash_deviations.py
@@ -301,7 +301,7 @@ def find_deviations(sc, reference, groups=None, signatures=None, min_support_dif
         'adapter_driver_version': list(all_app_notes) + list(all_gfx_critical_errors),
         'adapter_driver_version_clean': list(all_app_notes) + list(all_gfx_critical_errors),
         'CPU Info': ['cpu_microcode_version'],
-        'startup_crash': list(all_addons) + list(all_modules) + ['os_arch', 'shutdown_progress', 'safe_mode'],
+        'startup_crash': list(all_addons) + list(all_modules) + ['os_arch', 'shutdown_progress', 'safe_mode', 'ipc_channel_error'],
     }
 
     for addon in all_addons:
