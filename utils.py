2b8438d442d844a7d688f85055d62662065f0373
Marco Castelluccio <mcastelluccio@mozilla.com>
2017-01-11 16:26:19 +0100

    Move utility functions in the utils module

--- utils.py
+++ utils.py
@@ -1,3 +1,8 @@
+import os
+import errno
+import json
+import gzip
+import shutil
 from datetime import datetime
 import requests
 from requests.adapters import HTTPAdapter
@@ -15,3 +20,37 @@ def get_with_retries(url, params=None, headers=None):
     s.mount('https://crash-stats.mozilla.com', HTTPAdapter(max_retries=retries))
 
     return s.get(url, params=params, headers=headers)
+
+
+def mkdir(path):
+    try:
+        os.mkdir(path)
+    except OSError as e:
+        if e.errno != errno.EEXIST:
+            raise e
+
+
+def rmdir(path):
+    try:
+        shutil.rmtree(path)
+    except OSError as e:
+        if e.errno != errno.ENOENT:
+            raise e
+
+
+def write_json(path, obj):
+    with gzip.open(path, 'wb') as f:
+        json.dump(obj, f)
+
+
+def copytree(src, dst):
+    if not os.path.exists(dst):
+        os.makedirs(dst)
+
+    for item in os.listdir(src):
+        s = os.path.join(src, item)
+        d = os.path.join(dst, item)
+        if os.path.isdir(s):
+            copytree(s, d)
+        else:
+            shutil.copy2(s, d)
